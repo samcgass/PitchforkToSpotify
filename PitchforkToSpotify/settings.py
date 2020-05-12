e107452bfb7b6a934eebf8ab807ea416f9ea2a2f
Sam Gass <samcgass@gmail.com>
2020-05-11 20:01:01 -0500

    Changed templates to extend a base template rather than function as independent html

--- PitchforkToSpotify/settings.py
+++ PitchforkToSpotify/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'PitchforkToSpotify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/bamas/Documents/Personal Projects/DjangoPitchforkToSpotify/PitchforkToSpotify/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
