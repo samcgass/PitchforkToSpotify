236798924ec62f584e01c7a2b1aaa09fe59d6ac1
Sam Gass <samcgass@gmail.com>
2020-05-13 15:15:17 -0500

    added methods to update databases

--- databases/update.py
+++ databases/update.py
@@ -1,5 +1,6 @@
 from html.parser import HTMLParser
 from requests import get
+from databases.models import AllHighlyRatedAlbums, AllBestNewMusic
 
 
 class MyParser(HTMLParser):
@@ -33,18 +34,64 @@ def combine(list1, list2):
     return one_list
 
 
-def getArtistsAndAlbums(page):
+def getArtistsAndAlbums(all_highly_rated, page):
     parser = MyParser()
-    res = get("https://pitchfork.com/reviews/best/albums/?page=" + str(page))
+    if all_highly_rated:
+        res = get(
+            "https://pitchfork.com/best/high-scoring-albums/?page=" + str(page))
+    else:
+        res = get("https://pitchfork.com/reviews/best/albums/?page=" + str(page))
     parser.feed(res.text)
     artists_albums = combine(parser.artists, parser.albums)
     parser.close()
     return artists_albums, res.status_code
 
 
-def updateDatabases():
-    text_albums, status = getArtistsAndAlbums(1)
-    # do something
+def updateHighlyRatedDatabases():
+    text_albums, status = getArtistsAndAlbums(True, 1)
+    for album in text_albums:
+        try:
+            AllHighlyRatedAlbums.objects.get(album=album[1], artist=album[0])
+            return
+        except AllHighlyRatedAlbums.DoesNotExist:
+            alb = AllHighlyRatedAlbums(album=album[1], artist=album[0])
+            alb.save()
+    page_num = 2
     while status < 300:
-        text_albums, status = getArtistsAndAlbums()
-        # do somehting
+        text_albums, status = getArtistsAndAlbums(True, page_num)
+        for album in text_albums:
+            try:
+                AllHighlyRatedAlbums.objects.get(
+                    album=album[1], artist=album[0])
+                return
+            except AllHighlyRatedAlbums.DoesNotExist:
+                alb = AllHighlyRatedAlbums(album=album[1], artist=album[0])
+                alb.save()
+        page_num += 1
+
+
+def updateBestNewMusicDatabases():
+    text_albums, status = getArtistsAndAlbums(False, 1)
+    for album in text_albums:
+        try:
+            AllBestNewMusic.objects.get(album=album[1], artist=album[0])
+            return
+        except AllBestNewMusic.DoesNotExist:
+            alb = AllBestNewMusic(album=album[1], artist=album[0])
+            alb.save()
+    page_num = 2
+    while status < 300:
+        text_albums, status = getArtistsAndAlbums(True, page_num)
+        for album in text_albums:
+            try:
+                AllBestNewMusic.objects.get(album=album[1], artist=album[0])
+                return
+            except AllBestNewMusic.DoesNotExist:
+                alb = AllBestNewMusic(album=album[1], artist=album[0])
+                alb.save()
+        page_num += 1
+
+
+def updateDatabases():
+    updateBestNewMusicDatabases()
+    updateHighlyRatedDatabases()
