7ca5fd62e065b0282f8595d6e7f234f6a01db82e
Sam Gass <samcgass@gmail.com>
2020-05-13 17:03:24 -0500

    refactored update methods in database views

--- databases/views.py
+++ databases/views.py
@@ -10,18 +10,10 @@ def update(request):
 
 
 def updateHighlyRatedDatabases():
-    text_albums, status = getArtistsAndAlbums(True, 1)
-    for album in text_albums:
-        try:
-            AllHighlyRatedAlbums.objects.get(
-                album=album[1], artist=album[0])
-            return
-        except AllHighlyRatedAlbums.DoesNotExist:
-            alb = AllHighlyRatedAlbums(
-                album=album[1], artist=album[0], date=parse_datetime(album[2]))
-            alb.save()
-    page_num = 2
+    page_num = 1
+    status = 200
     while status < 300:
+        text_albums = []
         text_albums, status = getArtistsAndAlbums(True, page_num)
         for album in text_albums:
             try:
@@ -36,18 +28,10 @@ def updateHighlyRatedDatabases():
 
 
 def updateBestNewMusicDatabases():
-    text_albums, status = getArtistsAndAlbums(False, 1)
-    for album in text_albums:
-        try:
-            AllBestNewMusic.objects.get(
-                album=album[1], artist=album[0])
-            return
-        except AllBestNewMusic.DoesNotExist:
-            alb = AllBestNewMusic(
-                album=album[1], artist=album[0], date=parse_datetime(album[2]))
-            alb.save()
-    page_num = 2
+    page_num = 1
+    status = 200
     while status < 300:
+        text_albums = []
         text_albums, status = getArtistsAndAlbums(False, page_num)
         for album in text_albums:
             try:
